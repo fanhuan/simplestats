23552eeaff9807cf1fb6757aaa0826b1f8b5d8eb
Huan Fan <hfan22@wisc.edu>
2016-01-15 14:57:14 -0600

    final effort

--- stats.py
+++ stats.py
@@ -1,6 +1,13 @@
+import re
+
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals"""
     assert type(vals) is list, 'wrong input format'
+    vals = [float(x) for x in vals]
+    for item in vals:
+    	if re.match('^[0-9]*',item):???
+    		item = float(item)
+    	assert float(item)
     total = sum(vals)
     if total == 0:
     	return 0.0
@@ -11,12 +18,11 @@ def mean(vals):
 #print(mean([2,4]))
 #print(mean("hello"))
 
+#unit test: test functions
 def test_mean():
 	assert mean([2,4]) == 3.0
-	
 test_mean()
 
 def test_empty_list():
-	assert mean([]) == 0.0, 'emptylist'
-
+	assert mean([]) == 0.0
 test_empty_list()
--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,18 @@
 from stats import mean
+from nose.tools import assert_equal, assert_almost_equals
 
 def test_mean():
-	assert mean([2,4]) == 3.0
-test_mean()
+	assert_equal(mean([2,4]),3.0)
+#test_mean()
 
 def test_empty_list():
-	assert mean([]) == 0.0
-test_empty_list()
+	assert_equal(mean([]), 0.0)
+#test_empty_list()
 
 def test_float():
-	assert mean([1.1,2,0.5]) == 1.2
-test_float()
+	assert_almost_equals(mean([.5,1,.5]), 0.6666666666, 2) 
+	# the last argument is the decimal places to check
+#test_float()
+
+def test_str_list_mean():
+	assert_equal(mean(['1','2','3']),2.0)
